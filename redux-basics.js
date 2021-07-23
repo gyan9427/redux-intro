d96aa35089eec0ee7b80bbc2cc1631262195b82b
gyan <gyan9427@gmail.com>
2021-07-23 17:23:26 +0530

    Add subscription to the store

--- redux-basics.js
+++ redux-basics.js
@@ -30,6 +30,10 @@ const rootReducer = (state = initialState,action) => {
 const store = createStore(rootReducer);
 console.log(store.getState())
 
+// Subscription
+store.subscribe(()=>{
+    console.log('[Subscription]',store.getState())
+})
 
 // Dispatching action
 store.dispatch({type:'INC_COUNTER'});
