879537b002b5ae90e21c80ece104bfe46e1b0f5b
gyan <gyan9427@gmail.com>
2021-07-23 17:36:48 +0530

    Create a store in index.js create store component in containers and import it to index.js and pass it to store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux';
+import reducer from './containers/store/store';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(reducer); 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
