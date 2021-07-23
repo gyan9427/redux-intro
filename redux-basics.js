5ca875df33a63b0a28a46bbc447fd7537d309c5b
gyan <gyan9427@gmail.com>
2021-07-23 17:08:22 +0530

    dispatch the actions- INC_COUNTER & ADD_COUNTER

--- redux-basics.js
+++ redux-basics.js
@@ -11,9 +11,27 @@ const initialState = {
 // reducer requires two parameters a state and an action
 // but the also has to be initialized as it carries two conditions initial and final 
 const rootReducer = (state = initialState,action) => {
+    if(action.type === "INC_COUNTER") {
+        return{
+            ...state,
+            counter: state.counter + 1,
+    };
+    }
+    if(action.type === "ADD_COUNTER") {
+        return{
+            ...state,
+            counter: state.counter + 10,
+    };
+}
     return state;
 }
 
-//call the store and pass the reducer to it
+// call the store and pass the reducer to it
 const store = createStore(rootReducer);
+console.log(store.getState())
+
+
+// Dispatching action
+store.dispatch({type:'INC_COUNTER'});
+store.dispatch({type:'ADD_COUNTER',valur:10});
 console.log(store.getState())
